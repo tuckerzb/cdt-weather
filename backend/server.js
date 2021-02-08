42af6a58e34e5d5999415bb1d3509077a76df6ac
Zach Tucker <zbtucker@gmail.com>
2021-02-07 22:25:22 -0700

    add processs.env.PORT for Heroku

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,7 @@ app.use(morgan('combined'));
     })
 
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => (
     console.log(`Server listening on port ${PORT}`)
