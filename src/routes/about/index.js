6e3ba26abd0343e321721dcd85ce7b7d5ac4d186
Zach Tucker <zbtucker@gmail.com>
2021-02-19 12:53:25 -0700

    add Netlify redirects

--- src/routes/about/index.js
+++ src/routes/about/index.js
@@ -7,7 +7,7 @@ const About = () => (
 		<div class={style.headerBlock}>
 			<h1>About</h1>
 			<p>This project was inspired by <a href='https://www.atweather.org/' target='_blank' rel="noopener noreferrer">ATWeather.org</a> - an incredible resource for the AT and PCT hiking communities developed by Pat Jones.</p>
-			<p>CDT Weather is created and maintained by <a href='https://zachtucker.dev'>Zach "Free Fall" Tucker</a> (Tahoe Rim Trail 2019, AT NOBO 2020). Please use the <Link href='/contact'>Contact</Link> page for comments, suggestions, and concerns.</p>
+			<p>CDT Weather is created and maintained by <a href='https://zachtucker.dev' target='_blank' rel="noopener noreferrer">Zach "Free Fall" Tucker</a> (Tahoe Rim Trail 2019, AT NOBO 2020). Please use the <Link href='/contact'>Contact</Link> page for comments, suggestions, and concerns.</p>
 		</div>
 	</div>
 );
