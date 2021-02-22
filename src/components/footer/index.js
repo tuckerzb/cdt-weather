38918732ba63a8b3978b9a660949927aabfd38ff
Zach Tucker <zbtucker@gmail.com>
2021-02-21 19:58:08 -0700

    make author link open in new tab

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -5,7 +5,7 @@ import style from './style.css';
 const Footer = () => (
 	<footer class={style.footer}>
         <div class={style.footerCopyright}>Copyright &copy;2021</div>
-		<div class={style.footerContent}>Website by <a href='https://zachtucker.dev'>Zach "Free Fall" Tucker</a></div>
+		<div class={style.footerContent}>Website by <a target='_blank' rel="noopener noreferrer" href='https://zachtucker.dev'>Zach "Free Fall" Tucker</a></div>
 	</footer>
 );
 
