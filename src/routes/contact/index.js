ec89e927ce78d4f95717732daa8065c18ae6fdf9
Zach Tucker <zbtucker@gmail.com>
2022-04-16 10:56:26 -0600

    switch to netlify forms

--- src/routes/contact/index.js
+++ src/routes/contact/index.js
@@ -32,7 +32,6 @@ const Contact = () => {
       );
       return;
     }
-    console.log(`Sending Message`);
     axios({
       method: 'POST',
       url: `https://lit-shelf-99541.herokuapp.com/api/sendMessage`,
@@ -65,52 +64,53 @@ const Contact = () => {
       {response && <div class={style.messageBlock}>{response}</div>}
       {error && <div class={style.errorBlock}>{error}</div>}
       <div class={style.formContainer}>
-        <div>
-          <label for='name'>Your Name:</label>
-          <input
-            type='text'
-            id='name'
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </div>
-        <div>
-          <label for='name'>Your Email:</label>
-          <input
-            type='email'
-            id='email'
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <div>
-          <label for='name'>Your Message:</label>
-          <textarea
-            id='message'
-            value={message}
-            rows={5}
-            onChange={(e) => setMessage(e.target.value)}
-          />
-        </div>
-        <div>
-          <label for='botcheck'>
-            <strong>Bot Check: Select the CDT Terminus Country</strong>
-          </label>
-          <select
-            value={botCheck}
-            id='botcheck'
-            class={style.botCheck}
-            onChange={(e) => setBotCheck(e.target.value)}>
-            <option value=''>Please Select</option>
-            <option value='costarica'>Costa Rica</option>
-            <option value='russia'>Russia</option>
-            <option value='canada'>Canada</option>
-            <option value='france'>France</option>
-          </select>
-        </div>
-        <button type='submit' onClick={sendMessage}>
-          Send Message
-        </button>
+        <form name='contact' method='post'>
+          <input type='hidden' name='form-name' value='contact' />
+          <div>
+            <label for='name'>Your Name:</label>
+            <input
+              type='text'
+              id='name'
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div>
+            <label for='name'>Your Email:</label>
+            <input
+              type='email'
+              id='email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div>
+            <label for='name'>Your Message:</label>
+            <textarea
+              id='message'
+              value={message}
+              rows={5}
+              onChange={(e) => setMessage(e.target.value)}
+            />
+          </div>
+          <div>
+            <label for='botcheck'>
+              <strong>Bot Check: Select the CDT Terminus Country</strong>
+            </label>
+            <select
+              value={botCheck}
+              id='botcheck'
+              class={style.botCheck}
+              onChange={(e) => setBotCheck(e.target.value)}>
+              <option value=''>Please Select</option>
+              <option value='costarica'>Costa Rica</option>
+              <option value='russia'>Russia</option>
+              <option value='canada'>Canada</option>
+              <option value='france'>France</option>
+            </select>
+          </div>
+          <button type='submit'>Send Message</button>
+        </form>
       </div>
     </div>
   );
